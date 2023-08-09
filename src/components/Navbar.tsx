d452ef98b1a431e68a7639e698a3e7ba3b579a38
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2023-08-09 23:20:29 +0530

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { authModalState } from '@/atoms/authModalAtom';
+import { useSetRecoilState } from "recoil";
+
+type NavbarProps = {
+    
+};
+
+const Navbar:React.FC<NavbarProps> = () => {
+    const setAuthModalState = useSetRecoilState(authModalState)
+    const handleClick = () => {
+      setAuthModalState((prev)=>({...prev, isOpen:true}));
+    }
+    return <div className="flex items-center justfiy-between sm:px-12 px-2 md:px-24">
+        <a href="/" className="flex items-center justify-center h-20">
+        <img src="/logo.png" alt="LeetClone" className="h-full" />
+      </a>
+    <div className="flex items-center  ml-auto space-x-4">
+    <button className="bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-lg text-base font-medium hover:text-brand-orange
+     hover:bg-white hover:border-brand-orange border-2 border-transparent transition duration-300 ease-in-out "
+     onClick={handleClick}>
+        Sign In  
+        </button> </div>
+    </div>
+}
+export default Navbar;

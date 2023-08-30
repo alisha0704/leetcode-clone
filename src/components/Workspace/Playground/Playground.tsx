5ba2a53ca19ecb9d210db845487321ff132acb4f
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2023-08-30 16:44:53 +0530

    update: added test case headers

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -33,6 +33,17 @@ const Playground:React.FC<PlaygroundProps> = () => {
                         <hr className='absolute bottom-0 h-0.5 w-16 rounded-full border-none bg-white'/>
                     </div>
                 </div>
+                <div className='flex'>
+                   {/* case 1 */}
+                    <div className='mr-2 items-start mt-2 text-white'>
+                        <div className='flex flex-wrap items-center gap-y-4'>
+                            <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-3 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap '>
+                                Case 1
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 </div>
         </Split>
         </div>

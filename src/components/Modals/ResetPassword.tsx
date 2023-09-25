48c5dff57d2db5b26b6eba98e497aee85210a184
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2023-09-25 19:21:28 +0530

    fix: sizing

--- src/components/Modals/ResetPassword.tsx
+++ src/components/Modals/ResetPassword.tsx
@@ -28,7 +28,7 @@ const ResetPassword:React.FC<ResetPasswordProps> = () => {
 
         <button
             type='submit'
-            className={`w-full text-white  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
+            className={`w-full text-white  focus:ring-4 focus:ring-blue-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
             bg-brand-orange hover:bg-brand-orange-s `}
         >
             Reset My Password

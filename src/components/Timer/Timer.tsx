44f33d55a8d6b226c2870d0f6b85298cd04f8596
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2023-11-29 17:15:09 +0530

    fix: timer

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -30,7 +30,7 @@ const Timer:React.FC<TimerProps> = () => {
 	}, [showTimer]);
     return <div>
         {showTimer ? (
-            <div className='flex items-center space-x-4 bg-dark-fill-5 py-1.5 cursor-pointer rounded hover:bg-dark-fill-2'>
+            <div className='flex items-center space-x-6 bg-dark-fill-6 py-1.5 cursor-pointer rounded hover:bg-dark-fill-2'>
                <div>{formatTime(time)}</div>
             <FiRefreshCcw
             onClick={() => {
